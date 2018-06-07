9273cf99ce282be805be93aa4021348c4052d7be
Vivek Malhan <vivek.malhan@yahoo.com>
2018-06-07 12:13:52 -0400

    Final touching

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,18 +11,36 @@ var winSound = new Audio("./assets/sounds/you-win.wav");
 var lostSound = new Audio("./assets/sounds/you-lose.wav");
 
 // adding key press event to the document
-document.onkeyup = function(event) {
+document.onkeydown = function(event) {
     
     // press any key to start the function
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    keySound.play();
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        keySound.play();
+    }
+    // removing the guessed letter from compChoices array
+    function spliced() {
+        var a = compChoices.indexOf(userGuess);
+        if (a >= 0) {
+            compChoices.splice(a,1);
+        }
+     };
+     spliced();
     
     // computer guess is one of the computer choices
     var compGuess = compChoices[Math.floor(Math.random()* compChoices.length)];
     
     // pushing user guess in to guessesSoFar array
-    guessesSoFar.push(userGuess);
-    
+    function pushGuess(letter) {
+        if (guessesLeft > 0) {
+            if (guessesSoFar.indexOf(letter) === -1) {
+                guessesSoFar.push(letter);
+                guessesLeft--;
+            }
+        }
+    };
+    pushGuess(userGuess);
+
     // game starts
     if (userGuess == compGuess) {
         winSound.play();
@@ -35,20 +53,16 @@ document.onkeyup = function(event) {
         alert("You Lost ! Try Again !");
         guessesLeft = 9; //reset guesses left to 9
         guessesSoFar.length = 0;  //reset getSoFar array to be empty
-        losses++;
-    } else if (userGuess !== compGuess) {
-        guessesLeft--;
+        losses++; 
     };
-    
+
     // adding html through an event listner
-    newFunction(userGuess);
-};
-function newFunction(userGuess) {
     document.querySelector("#user_Guess").innerHTML = userGuess;
     document.querySelector("#wins").innerHTML = wins;
     document.querySelector("#losses").innerHTML = losses;
     document.querySelector("#guess_Left").innerHTML = guessesLeft;
     document.querySelector("#guess_Arr").innerHTML = guessesSoFar;
 };
+
 
 
